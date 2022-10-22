8637b17feb99176b8f19779b26f19d87403acc64
kusanob <kusanob@email.com>
2022-10-22 07:20:19 +0000

    finish_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destyoy]
+  before_action :correct_user, only: [:edit, :uodate]
+  before_action :admin_user, only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -19,10 +26,48 @@ class UsersController < ApplicationController
       end
   end  
   
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "アカウント情報を更新しました"
+      redirect_to user_url
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました"
+    redirect_to @user
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    redirect_to root_url unless current_user?(@user)
+  end
+  
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
   
 end
