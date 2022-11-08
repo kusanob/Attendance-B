31bc3bf8e92b1ed8f20df6927de08c443cbe3c51
kusanob <kusanob@email.com>
2022-11-08 10:06:05 +0000

    all finish

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,7 +2,9 @@ class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_week]
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
-  before_action :set_one_month, only: :edit_one_month
+  before_action :set_one_month, only: :edit_one_month, on: :update_one_month
+  
+  
   
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
@@ -31,20 +33,19 @@ class AttendancesController < ApplicationController
   def update_one_month
     ActiveRecord::Base.transaction do
       attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        if item[:started_at].present? && item[:finished_at].present?
-          attendance.update_attributes!(item)
-          flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-          redirect_to user_url(date: params[:date])
-        else
+        if item[:started_at].present? && item[:finished_at].blank?
           flash[:danger] = "退勤も更新してください"
           redirect_to attendances_edit_one_month_user_url(date: params[:date])and return
         end
+          attendance = Attendance.find(id)
+          attendance.update_attributes!(item)
       end
     end
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])and return
   rescue ActiveRecord::RecordInvalid
-      flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
-      redirect_to attendances_edit_one_month_user_url(date: params[:date])
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
 
   private
@@ -62,12 +63,6 @@ class AttendancesController < ApplicationController
     end
 end
 
-# @attendance.started_at.present? && @attendance.finished_at.present?
-# @attendance.update_attributes(started_at: Time.current, finished_at: Time.current)
-# flash[:danger] = "退勤も更新してください"
-      # redirect_to attendances_edit_one_month_user_url(date: params[:date])and return
-# @attendance = Attendance.find(params[:id])
-    # if @attendance.started_at.present? && @attendance.finished_at.blank?
-      # flash[:danger] = "退勤も更新してください"
-      # redirect_to attendances_edit_one_month_user_url(date: params[:date])and return
-    # end
+# else
+          # flash[:danger] = "退勤も更新してください"
+          # redirect_to attendances_edit_one_month_user_url(date: params[:date])and return
